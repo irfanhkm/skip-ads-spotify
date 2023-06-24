fa2899d146910fe0eff5f08ce2f8ecf466622c05
Irfan Hakim <hakimirfan90@gmail.com>
2023-06-24 22:44:57 +0700

    add precheck condition spotify running or not, and reformat code

--- services/darwin/osascript.js
+++ services/darwin/osascript.js
@@ -1,5 +1,11 @@
 const { exec } = require('child_process');
 
+const Script = {
+	GetPlayerState: 'tell application "Spotify" to get player state',
+    GetCurrentTrack: 'tell application "Spotify" to if player state is playing then return {name, artist, album} of current track',
+    UpdateVolume: 'tell application "Spotify" to set sound volume to '
+}
+
 function executeCommand(command) {
     return new Promise((resolve, reject) => {
         exec(`osascript -e \'${command}\'`, (error, stdout, stderr) => {
@@ -10,7 +16,9 @@ function executeCommand(command) {
                 resolve(stdout.trim());
             }
         });
+    }).catch((error) => {
+        console.error(`Command execution error: ${error}`);
     });
 }
 
-module.exports = { executeCommand };
+module.exports = { executeCommand, Script };
--- services/darwin/trackService.js
+++ services/darwin/trackService.js
@@ -1,47 +1,75 @@
-const { executeCommand } = require('./osascript');
+const { executeCommand, Script } = require('./osascript');
+const { exec } = require('child_process');
 
 
 let currentMedia = {};
 let statePlayer;
 let isCurrStateAds = false;
+let isPlayerRunning;
 
 const statePlaying = "playing";
 const advertisementTag = "Advertisement";
-const defaultVolumeAfterAds = 20;
+const defaultVolumeAfterAds = 100;
 const zeroVolume = 0;
 
 async function fetchMetadata() {
-    statePlayer = await executeCommand('tell application "Spotify" to get player state');
+    const playerRunning = await checkSpotifyRunning();
 
-    if (statePlayer === null || statePlayer != statePlaying) {
+    if (!playerRunning) {
+        if (isPlayerRunning !== playerRunning) {
+            currentMedia = {};
+            console.log('no music player is running');
+        }
+
+        isPlayerRunning = false;
+        return;
+    }
+    isPlayerRunning = true;
+
+    statePlayer = await executeCommand(Script.GetPlayerState);
+    if (statePlayer === null || statePlayer !== statePlaying) {
         return;
     }
 
-    const trackInfo = await executeCommand('tell application "Spotify" to if player state is playing then return {name, artist, album} of current track');
-
-    if (trackInfo == null) {
+    const trackInfo = await executeCommand(Script.GetCurrentTrack);
+    if (trackInfo === null) {
         return;
     }
 
     const [title, artist, album] = trackInfo.trim().split(', ');
-
-    if (currentMedia.title !== title || currentMedia.artist !== artist || currentMedia.album !== album) {
+    if (
+        currentMedia.title !== title 
+        || currentMedia.artist !== artist
+        || currentMedia.album !== album
+    ) {
         currentMedia = { title, artist, album };
 
         if (title === advertisementTag) {
             console.log(`Now is advertisement, we keep volume down`);
-            await executeCommand(`set volume output volume ${zeroVolume}`);
+            await executeCommand(`${Script.UpdateVolume} ${zeroVolume}`);
             isCurrStateAds = true;
             return;
         } else if (isCurrStateAds) {
             console.log(`Advertisement complete, we set the default volume`);
-            await executeCommand(`set volume output volume ${defaultVolumeAfterAds}`);
+            await executeCommand(`${Script.UpdateVolume} ${defaultVolumeAfterAds}`);
             isCurrStateAds = false;
         }
 
-        // Handle the media metadata change
         console.log(`Current song is ${title} by ${artist} on album ${album}`);
     }
+}
+
+function checkSpotifyRunning() {
+    const command = 'pgrep -x Spotify';
+
+    return new Promise((resolve) => {
+        exec(command, (error, stdout, stderr) => {
+            resolve(stdout != null && stdout.length > 0);
+        });
+    }).catch((error) => {
+        console.error(`Error checking if Spotify is running: ${error}`);
+        return false;
+    });
 }
 
 module.exports = { fetchMetadata };
