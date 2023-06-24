90ba30d6fe53e94931dfd214ab4c49d0db88a7a3
Irfan Hakim <hakimirfan90@gmail.com>
2023-06-24 21:51:11 +0700

    add exception flow for darwin only

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const { app } = require('electron');
-const { fetchMetadata } = require('./services/trackService');
+const { fetchMetadata } = require('./services/darwin/trackService');
 
 const intervalFetchMetadata = 2000;
 
 function createWindow() {
-  // Call fetchMetadata initially
-  fetchMetadata();
+  const os = process.platform;
+  if (os === 'darwin') {
+    // Call fetchMetadata initially
+    fetchMetadata();
 
-  // Call fetchMetadata at the specified interval
-  setInterval(fetchMetadata, intervalFetchMetadata);
+    // Call fetchMetadata at the specified interval
+    setInterval(fetchMetadata, intervalFetchMetadata);
+  } else {
+    console.log('bye, we will support for your os');
+    app.exit(0);
+  }
 }
 
 app.on('ready', () => {
